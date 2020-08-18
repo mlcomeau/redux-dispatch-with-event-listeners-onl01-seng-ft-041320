b3f87359c7e465f56fc701c7ff4e80e08627c224
Megan Comeau <mlcomeau99@gmail.com>
2020-08-18 20:08:32 +0000

    Automatically backed up by Learn

--- js/reducer.js
+++ js/reducer.js
@@ -8,21 +8,21 @@ function reducer(state = {count: 0}, action){
       return state;
   }
 }
- 
+
 function dispatch(action){
   state = reducer(state, action);
   render();
 }
- 
+
 function render(){
   let container = document.getElementById('container');
   container.textContent = state.count;
 }
- 
+
 dispatch({type: '@@INIT'})
- 
+
 let button = document.getElementById('button');
- 
+
 button.addEventListener('click', () => {
   dispatch({type: 'INCREASE_COUNT'})
 })
